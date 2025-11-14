1b5897e15283f52e2c1e6690cfc17a4f66375b09
Cristian Camacho <criistian13062000@gmail.com>
2025-11-13 23:32:52 -0300

    mbed ejercicio 1

--- mbed-lpc1768/apps/ej1/main.cpp
+++ mbed-lpc1768/apps/ej1/main.cpp
@@ -1,14 +1,20 @@
 #include "mbed.h"
 
 DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
 
-int main()
-{
-    while(1)
-    {
-        led1 = 0;
-        wait(1);
-        led1 = 1;
-        wait(1);
+int main() {
+    while (1) {
+        for (int i = 0; i < 16; i++) {
+
+            led1 = (i / 1) % 2;     // bit 0 
+            led2 = (i / 2) % 2;     // bit 1
+            led3 = (i / 4) % 2;     // bit 2
+            led4 = (i / 8) % 2;     // bit 3
+
+            wait(1); 
+        }
     }
 }
