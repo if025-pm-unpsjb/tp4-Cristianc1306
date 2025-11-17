ba31f1752bc0db1064daf2ca6d6b8f03eec089ae
Cristian Camacho <criistian13062000@gmail.com>
2025-11-17 16:59:53 -0300

    bed ejercicio 4

--- mbed-lpc1768/apps/ej4/main.cpp
+++ mbed-lpc1768/apps/ej4/main.cpp
@@ -1,14 +1,19 @@
 #include "mbed.h"
 
-DigitalOut led1(LED1);
+DigitalOut leds[] = {LED1, LED2, LED3, LED4};
 
-int main()
-{
-    while(1)
-    {
-        led1 = 0;
-        wait(1);
-        led1 = 1;
-        wait(1);
+Serial pc(USBTX, USBRX);
+
+int main() {
+    pc.printf("Ingrese un numero de LED (1 a 4):");
+    int led_num;
+    while (1) {
+        if (pc.readable()) {  
+            char c = pc.getc(); 
+            pc.putc(c); 
+            led_num = c - '1';
+        }
+        leds[led_num] = !leds[led_num];
+        wait_ms(250);
     }
 }
