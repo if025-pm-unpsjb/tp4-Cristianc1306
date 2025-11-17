482dc6f333de9f5423d56b80de393d93a40022e1
Cristian Camacho <criistian13062000@gmail.com>
2025-11-17 17:00:04 -0300

    bed ejercicio 5

--- mbed-lpc1768/apps/ej5/main.cpp
+++ mbed-lpc1768/apps/ej5/main.cpp
@@ -1,14 +1,49 @@
 #include "mbed.h"
 
 DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
+Serial pc(USBTX, USBRX);  
 
-int main()
-{
-    while(1)
-    {
-        led1 = 0;
-        wait(1);
-        led1 = 1;
-        wait(1);
+int intervalo = 500;  
+char buffer[10];
+int indice = 0;
+
+void leerIntervalo() {
+
+    char c = pc.getc();
+    pc.putc(c);
+
+    if (c == '\r') {  
+    	pc.printf("\n");
+        buffer[indice] = '\0';      
+        intervalo = atoi(buffer);  
+        indice = 0;
+    } 
+    else  {
+        buffer[indice++] = c;
+    }    
+}
+
+int main() {
+
+    pc.printf("Ingrese un intervalo en ms (por ejemplo: 500):");
+
+    while (1) {
+
+        for (int i = 0; i < 16; i++) {
+            led1 = (i / 1) % 2;   // bit 0
+            led2 = (i / 2) % 2;   // bit 1
+            led3 = (i / 4) % 2;   // bit 2
+            led4 = (i / 8) % 2;   // bit 3
+
+            wait_ms(intervalo);
+
+            if (pc.readable()) {  
+                leerIntervalo();
+            }    
+        }
     }
 }
+
