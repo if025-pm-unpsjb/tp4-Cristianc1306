53e6024219f692db73c3c42034fd09bf7e60197b
Cristian Camacho <criistian13062000@gmail.com>
2025-11-19 22:06:25 -0300

    edu-ciaa ejercicio 2

--- edu-ciaa-nxp/apps/ej2/src/app.c
+++ edu-ciaa-nxp/apps/ej2/src/app.c
@@ -8,15 +8,42 @@
 #include "app.h"         // <= Su propia cabecera (opcional)
 #include "sapi.h"        // <= Biblioteca sAPI
 
-int main( void )
-{
-        // Inicializar y configurar la placa de desarrollo
-        boardConfig();
+int main(void) {
 
-        while( TRUE ) {
-                gpioWrite( CIAA_BOARD_LED, 1 );
-                delay(1000);
-                gpioWrite( CIAA_BOARD_LED, 0 );
-                delay(1000);
+    boardConfig();
+    uartConfig(UART_USB, 9600);
+
+    while(TRUE) {
+
+
+        while(TRUE) {
+
+            if(!gpioRead(TEC1)) {
+                gpioToggle(CIAA_BOARD_LED);
+                printf("BOTON1\r\n");
+                delay(300);
+            }
+
+            if(!gpioRead(TEC2)) {
+                gpioToggle(LED1);
+                printf("BOTON2\r\n");
+                delay(300);
+            }
+
+            if(!gpioRead(TEC3)) {
+                gpioToggle(LED2);
+                printf("BOTON3\r\n");
+                delay(300);
+            }
+
+            if(!gpioRead(TEC4)) {
+                gpioToggle(LED3);
+                printf("BOTON4\r\n");
+                delay(300);
+            }
         }
+
+    }
+
+    return 0;
 }
