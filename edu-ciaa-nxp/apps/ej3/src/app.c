53102ddf7a34a20696aaad725353f341c0dace69
Cristian Camacho <criistian13062000@gmail.com>
2025-11-19 22:06:35 -0300

    edu-ciaa ejercicio 3

--- edu-ciaa-nxp/apps/ej3/src/app.c
+++ edu-ciaa-nxp/apps/ej3/src/app.c
@@ -8,15 +8,57 @@
 #include "app.h"         // <= Su propia cabecera (opcional)
 #include "sapi.h"        // <= Biblioteca sAPI
 
-int main( void )
-{
-        // Inicializar y configurar la placa de desarrollo
-        boardConfig();
 
-        while( TRUE ) {
-                gpioWrite( CIAA_BOARD_LED, 1 );
-                delay(1000);
-                gpioWrite( CIAA_BOARD_LED, 0 );
-                delay(1000);
+int main(void) {
+
+    boardConfig();
+    uartConfig(UART_USB, 9600);
+
+    bool_t ledR = OFF;
+    bool_t ledG = OFF;
+    bool_t ledB = OFF;
+
+    bool_t rgbEnabled = OFF;   
+
+    while(TRUE) {
+
+        if(!gpioRead(TEC1)) {
+            rgbEnabled = !rgbEnabled;
+            printf("BOTON1\r\n");
+            delay(200);
         }
+
+        if(!gpioRead(TEC2)) {
+            ledR = !ledR;
+            printf("BOTON2\r\n");
+            delay(200);
+        }
+
+        if(!gpioRead(TEC3)) {
+            ledG = !ledG;
+            printf("BOTON3\r\n");
+            delay(200);
+        }
+
+        if(!gpioRead(TEC4)) {
+            ledB = !ledB;
+            printf("BOTON4\r\n");
+            delay(200);
+        }
+
+        if(rgbEnabled) {
+            gpioWrite(LEDR, ledR);
+            gpioWrite(LEDG, ledG);
+            gpioWrite(LEDB, ledB);
+        } else {
+            gpioWrite(LEDR, OFF);
+            gpioWrite(LEDG, OFF);
+            gpioWrite(LEDB, OFF);
+        }
+
+        delay(20);
+    }
+
+    return 0;
 }
+
