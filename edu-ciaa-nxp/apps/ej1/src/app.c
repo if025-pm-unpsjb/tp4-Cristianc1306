2df924f4e3e5d45b43a43fcc7e95ca5f873cb448
Cristian Camacho <criistian13062000@gmail.com>
2025-11-19 22:06:14 -0300

    edu-ciaa ejercicio 1

--- edu-ciaa-nxp/apps/ej1/src/app.c
+++ edu-ciaa-nxp/apps/ej1/src/app.c
@@ -13,10 +13,28 @@ int main( void )
         // Inicializar y configurar la placa de desarrollo
         boardConfig();
 
+        uartConfig(UART_USB, 9600); 
+
+        char buffer[32];
+        uint8_t index = 0;
+
         while( TRUE ) {
-                gpioWrite( CIAA_BOARD_LED, 1 );
-                delay(1000);
-                gpioWrite( CIAA_BOARD_LED, 0 );
-                delay(1000);
+
+            if(uartReadByte(UART_USB, &buffer[index])) {
+
+                if(buffer[index] == '\r') {
+
+                   buffer[index + 1] = '\0'; 
+
+                   uartWriteString(UART_USB, buffer);
+                   uartWriteString(UART_USB, "\r\n");
+
+                   index = 0;
+                }
+                else {
+                   index++;
+                }
+            }
+        
         }
 }
